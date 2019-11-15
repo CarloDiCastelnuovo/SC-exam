55e46ad9fd41e8cd51ce5a554f1ed45dc98fec9d
Carlo Di Castelnuovo <carlo.dicastelnuovo@studio.unibo.it>
2019-11-15 17:00:50 +0100

    Il codice procede

--- GitFile.py
+++ GitFile.py
@@ -8,12 +8,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#Import the data from excel (or any other) and get the DataFrame 
 data = pd.read_excel("c:/Users/carlausss/Desktop/Prova.xlsx")
 data = data.dropna()
-
 data = pd.DataFrame(data)
 
+
+#Define the functions to get close points
 def get_close_points(df, x, y, radius = 2):
     x_idx = data.iloc[:, 0]
     y_idx = data.iloc[:, 1]
@@ -22,7 +23,7 @@ def get_close_points(df, x, y, radius = 2):
     
     return data.loc[to_take]
  
-
+#Create empty matrices of the correct lenght to be filled
 a = np.empty(((max(data.iloc[:,0])) + 1,(max(data.iloc[:,1])) + 1))
 a.fill(np.nan)
 b = np.empty(((max(data.iloc[:,0])) + 1,(max(data.iloc[:,1])) + 1))
@@ -38,31 +39,31 @@ i = 0
 
 while i < len(data):
    
-    xp = data.iloc[i,0]
+    xp = data.iloc[i,0]             #Positions on the matrix
     yp = data.iloc[i,1] 
 
-    alfa = data.iloc[i,2]
+    alfa = data.iloc[i,2]            #Values 
     beta = data.iloc[i,3]
 
-    a[int(xp), int(yp)] = alfa
+    a[int(xp), int(yp)] = alfa          #Fill the frist two matrices 
     b[int(xp), int(yp)] = beta
     
     parda = get_close_points(data, xp, yp, radius = 2)
-        
-    al = parda.iloc[:,2]
-    bet = parda.iloc[:,3]
+                                #Get the close points for every (xp,yp) couple 
+    al = parda.iloc[:,2]       #and collect their Alfa and Beta values in 2 different
+    bet = parda.iloc[:,3]      #pandas DataFrame
     df_al = pd.DataFrame(al)
     df_bet = pd.DataFrame(bet)
 
-    alfa_med = df_al.mean()
+    alfa_med = df_al.mean()          #Compute the mean of both the DataFrames
     beta_med= df_bet.mean()
     
-    c[int(xp), int(yp)] = alfa_med
+    c[int(xp), int(yp)] = alfa_med   #Fill the matrices with averaged values
     d[int(xp), int(yp)] = beta_med
     
     i = i + 1
 
-mat=[a,b,c,d]
+mat=[a,b,c,d]                        #Print subplots with all the matrices
 
 fig = plt.figure(figsize=(15,15))
 
