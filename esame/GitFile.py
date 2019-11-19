80d66e549ff56acd3d16721f56c2c75486e772d4
DrCarlaus <52068202+DrCarlaus@users.noreply.github.com>
2019-11-19 15:29:57 +0100

    Committo

--- esame/GitFile.py
+++ esame/GitFile.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 15:17:03 2019
-
-@author: carlausss
-"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,14 +7,13 @@ from sklearn.mixture import GaussianMixture
 
     #Import the data from excel (or any other) and get the DataFrame 
 
-#data = pd.read_excel("c:/Users/carlausss/Desktop/Prova.xlsx")
-#data = data.dropna()
-#data = pd.DataFrame(data)
-
+data = pd.read_excel("c:/Users/carlausss/Desktop/Prova.xlsx")
+data = data.dropna()
+data = pd.DataFrame(data)
             #Create random DataFrame
 
-a = np.random.randn(100,4)
-data = pd.DataFrame(a, columns=['x','y','Alfa','Beta'])
+#a = np.random.randn(100,4)
+#data = pd.DataFrame(a, columns=['x','y','Alfa','Beta'])
 
         #Define the functions to get close points
 
@@ -40,103 +33,103 @@ def km(df, nc = 2 ):
     labels_alfa = km_alfa.labels_ 
     labels_beta = km_beta.labels_ 
 
-    km_dfa = pd.DataFrame(labels_alfa)
-    km_dfb = pd.DataFrame(labels_beta)
+    return labels_alfa, labels_beta
 
-    return km_dfa, km_dfb
+
                             #GMM
-
 def gmm(df, nc = 2):
     gmm_alfa = GaussianMixture(n_components = nc).fit(data.iloc[:, 2:3])
     gmm_beta = GaussianMixture(n_components = nc).fit(data.iloc[:, 3:4])
 
     lab_alfa = gmm_alfa.predict(data.iloc[:, 2:3])
     lab_beta = gmm_beta.predict(data.iloc[:, 3:4])
-    
-    gmm_dfa = pd.DataFrame(lab_alfa)
-    gmm_dfb = pd.DataFrame(lab_beta)
 
-    return gmm_dfa, gmm_dfb
+    return lab_alfa, lab_beta
+
  
     #Create empty matrices of the correct lenght to be filled
 
-a = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-a.fill(np.nan)
-b = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-b.fill(np.nan)
-c = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-c.fill(np.nan)
-d = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-d.fill(np.nan)
-e = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-e.fill(np.nan)
-f = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-f.fill(np.nan)
-g = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-g.fill(np.nan)
-h = np.empty((int((max(data.iloc[:,0]))) + 1,int((max(data.iloc[:,1]))) + 1))
-h.fill(np.nan)
+def image(df):
 
+    m = []  
+    maxx=max(data.iloc[:,0])
+    maxy=max(data.iloc[:,1])
 
-i = 0
+    for i in range(8):
+        mat = np.empty((int(maxx),int(maxy)))
+        mat.fill(np.nan)        
+        
+        m.append(mat)
+        
+        i = 0
 
-while i < len(data):
-   
-    xp = data.iloc[i,0]             #Positions on the matrix
-    yp = data.iloc[i,1] 
+    while i < len(data):
+        
+        xp = data.iloc[i,0]             #Positions on the matrix
+        yp = data.iloc[i,1] 
 
-    alfa = data.iloc[i,2]            #Values 
-    beta = data.iloc[i,3]
+        alfa = data.iloc[i,2]            #Values 
+        beta = data.iloc[i,3]
 
-    a[int(xp), int(yp)] = alfa          #Fill the frist two matrices 
-    b[int(xp), int(yp)] = beta
+        m[0][int(xp), int(yp)] = alfa          #Fill the frist two matrices 
+        m[1][int(xp), int(yp)] = beta
     
-    parda = get_close_points(data, xp, yp, radius = 2)
+        parda = get_close_points(data, xp, yp, radius = 2)
                                 #Get the close points for every (xp,yp) couple 
-    al = parda.iloc[:,2]       #and collect their Alfa and Beta values in 2 different
-    bet = parda.iloc[:,3]      #pandas DataFrame
-    df_al = pd.DataFrame(al)
-    df_bet = pd.DataFrame(bet)
+        al = parda.iloc[:,2]       #and collect their Alfa and Beta values in 2 different
+        bet = parda.iloc[:,3]      #pandas DataFrame
+        df_al = pd.DataFrame(al)
+        df_bet = pd.DataFrame(bet)
 
-    alfa_med = df_al.mean()          #Compute the mean of both the DataFrames
-    beta_med= df_bet.mean()
+        alfa_med = df_al.mean()          #Compute the mean of both the DataFrames
+        beta_med= df_bet.mean()
     
-    c[int(xp), int(yp)] = alfa_med   #Fill the matrices with averaged values
-    d[int(xp), int(yp)] = beta_med
+        m[2][int(xp), int(yp)] = alfa_med   #Fill the matrices with averaged values
+        m[3][int(xp), int(yp)] = beta_med
+       
+        kma, kmb = km(data, nc=2)
+        gmma, gmmb = gmm(data, nc=2)
     
-    e[int(xp), int(yp)] = km_dfa.iloc[i,0]     #Clustering matrices 
-    f[int(xp), int(yp)] = km_dfb.iloc[i,0]
-
-    g[int(xp), int(yp)] = gmm_dfa.iloc[i,0]
-    h[int(xp), int(yp)] = gmm_dfb.iloc[i,0]
+        kma = pd.DataFrame(kma)
+        kmb = pd.DataFrame(kmb)
     
-    i = i + 1
-
-              #Print subplots with all the matrices
-
-mat=[a,b,c,d,e,f,g,h]          
-fig = plt.figure(figsize=(15,15))
-ax = []
-
-for i in range(8):
+        gmma = pd.DataFrame(gmma)
+        gmmb = pd.DataFrame(gmmb)
     
-    img = mat[i]
-    ax.append( fig.add_subplot(4, 2, i+1) )
+        m[4][int(xp), int(yp)] = kma.iloc[i,0]     #Clustering matrices 
+        m[5][int(xp), int(yp)] = kmb.iloc[i,0]
+
+        m[6][int(xp), int(yp)] = gmma.iloc[i,0]
+        m[7][int(xp), int(yp)] = gmmb.iloc[i,0]
     
-    ax[-1].set_xlabel('X')
-    ax[-1].set_ylabel('Y')    
+        i = i + 1
+        
+        return m
+   
+def print_image(m):           #Print subplots with all the matrices
     
-    plt.imshow(img)
+    fig = plt.figure(figsize=(15,15))
+    ax = []
+
+    for i in range(8):
+    
+        ax.append( fig.add_subplot(4, 2, i+1) )
+    
+        ax[-1].set_xlabel('X')
+        ax[-1].set_ylabel('Y')    
+    
+        images = plt.imshow(m[i])
 
 
-ax[0].set_title("Alfa Values")
-ax[1].set_title("Beta Values")
-ax[2].set_title("Averaged Alfa Values")
-ax[3].set_title("Averaged Beta Values")
-ax[4].set_title("KM Cluster on Alfa Values")
-ax[5].set_title("KM Cluster on Beta Values")
-ax[6].set_title("GMM Cluster on Alfa Values")
-ax[7].set_title("GMM Cluster on Beta Values")
+    ax[0].set_title("Alfa Values")
+    ax[1].set_title("Beta Values")
+    ax[2].set_title("Averaged Alfa Values")
+    ax[3].set_title("Averaged Beta Values")
+    ax[4].set_title("KM Cluster on Alfa Values")
+    ax[5].set_title("KM Cluster on Beta Values")
+    ax[6].set_title("GMM Cluster on Alfa Values")
+    ax[7].set_title("GMM Cluster on Beta Values")
+    
+    return images
 
-
-
+print_image(image(data))
