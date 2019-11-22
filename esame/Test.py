9b22f5ecac6c13bee111a44c7f01caa0bac1f481
DrCarlaus <52068202+DrCarlaus@users.noreply.github.com>
2019-11-22 16:24:08 +0100

    Add files via upload

--- esame/Test.py
+++ esame/Test.py
@@ -15,17 +15,25 @@ df2 = pd.DataFrame(np.random.randint(-100,0,size=(100, 5)), columns=('X', 'Y','A
    
 df3 = pd.DataFrame(np.random.uniform(size = (100,5)), columns=('X', 'Y','Alfa','Beta', 'Gamma'))
     
+
 def t_df(df):
     
     min_x=min(df.iloc[:,0])
     min_y=min(df.iloc[:,1])
     
     if min_x < 1:
-        raise ValueError('Wrong coordinates value')
+        raise ValueError('Wrong coordinates value ', min_x,' is not correct')
     
     if min_y < 1:
-        raise ValueError('Wrong coordinates value')  
+        raise ValueError('Wrong coordinates value ', min_y,' is not correct')  
+    
+    for i in range(len(df)):
         
+        if df.iloc[i,:] == np.nan:
+            raise ValueError ('All the columns must have the same lenght')
+        elif df.iloc[:,i] == np.nan:
+            raise ValueError ('All the columns must have the same lenght')
+    
 def test_function_1():
     
     t_df(df1)
@@ -38,6 +46,8 @@ def test_function_2():
 def test_function_3():
     
     t_df(df3)
-
+    
 test_function_1()
+test_function_4()
+
 
