803fd867c8d997482086bb8a9df726d754832dba
DrCarlaus <52068202+DrCarlaus@users.noreply.github.com>
2020-01-16 16:37:05 +0100

    Uploading

--- esame/Test.py
+++ esame/Test.py
@@ -9,6 +9,9 @@ import numpy as np
 import pandas as pd
 import pytest
 
+from Images import t_df, km, gmm, get_close_points
+
+
 df1 = pd.DataFrame(np.random.randint(1,100,size=(100, 5)), columns=('X', 'Y','Alfa','Beta', 'Gamma'))
    
 df2 = pd.DataFrame(np.random.randint(-100,0,size=(100, 5)), columns=('X', 'Y','Alfa','Beta', 'Gamma'))
@@ -16,31 +19,49 @@ df2 = pd.DataFrame(np.random.randint(-100,0,size=(100, 5)), columns=('X', 'Y','A
 df3 = pd.DataFrame(np.random.uniform(size = (100,5)), columns=('X', 'Y','Alfa','Beta', 'Gamma'))
     
 
-def t_df(df):
-    
-    min_x=min(df.iloc[:,0])
-    min_y=min(df.iloc[:,1])
-    
-    if min_x < 1:
-        raise ValueError('Wrong coordinates value ', min_x,' is not correct')
-    
-    if min_y < 1:
-        raise ValueError('Wrong coordinates value ', min_y,' is not correct')  
+#data = pd.read_excel("c:/Users/carlausss/Desktop/Prova.xlsx")
+#data = pd.DataFrame(data)
 
-    
+
 def test_function_1():
     
     t_df(df1)
-    print("DataFrame df1: \n",df1.head())
+    print("\nCorrect DataFrame shape df1: \n\n",df1.head())
     
-def test_function_2():
+    #Testing that t_df function raises error with wrong DataFrames
     
-    t_df(df2)
+    with pytest.raises(ValueError):
+        
+        t_df(df2)
+        
+        t_df(df3)
+        
 
-def test_function_3():
-    
-    t_df(df3)
-    
 test_function_1()
 
+
+def test_get_close_points():
+    
+    #Testing g_c_p raises ValueError for negative radius values
+    
+    with pytest.raises(ValueError):
+        get_close_points(df1, radius = -2)
+
+
+def test_km():
+    
+    #Testing km works correctly
+    
+    x = km(df1)
+    
+    assert len(x) == len(df1)
+    
+
+def test_gmm():
+    
+    #Testing gmm works correctly
+    
+    x = gmm(df1)
+    
+    assert len(x) == len(df1)
 
